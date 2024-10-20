402b2a75df709255bd1d2a3ff5220c53727f3808
fumimelvindev <fumimelvindev@gmail.com>
2024-10-20 21:18:04 +0200

    Save user to database after logging in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
+import { getCurrentUser } from "@/lib/checkUser";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await getCurrentUser();
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Welcome, User</a>
+        <a className="btn btn-ghost text-xl">
+          {user ? `Welcome, ${user?.name}` : "TICKET LEAD"}
+        </a>
       </div>
       <div className="flex-none">
         <SignedOut>
